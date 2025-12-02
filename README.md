e258989b88b9116d3c8791c140d948474d02c4b0
Oto Brglez <otobrglez@gmail.com>
2025-12-02 09:52:00 +0100

    Adding day 2.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This is my AoC 2025 in Scala 3 with Scala CLI.
 ## Solutions
 
 - [Day 1](./src/aoc/Main01.scala)
+- [Day 2](./src/aoc/Main02.scala)
 
 \- [Oto Brglez](https://github.com/otobrglez)
 
---
+++ src/aoc/Main02.scala
@@ -0,0 +1,52 @@
+package aoc
+
+import zio.*
+import zio.stream.*
+
+import java.nio.file.{Files, Path}
+import scala.util.Try
+
+object Main02 extends AOCApp:
+  private type ID              = BigInt
+  private type IDRange         = (ID, ID)
+  private type NumberValidator = ID => Boolean
+
+  final private case class LeadingZeroError(raw: String)              extends Throwable:
+    override def getMessage: String = s"Leading zeroes are not allowed: $raw"
+  final private case class RepeatedNumbersError(invalidIDs: List[ID]) extends Throwable:
+    override def getMessage: String = s"Invalid IDs: ${invalidIDs.mkString(",")}"
+  final private case class InvalidNumber(raw: String)                 extends Throwable:
+    override def getMessage: String = s"Invalid number/range/input: $raw"
+
+  private def parseRange(numberValidator: NumberValidator)(raw: String): Either[Throwable, IDRange] = for
+    List(rawStart, rawEnd) <- Try(raw.split("-", 2).map(_.trim).toList).toEither.left.map(_ => InvalidNumber(raw))
+    startID                <- Either.cond(!(rawStart.startsWith("0") && rawStart.length > 1), rawStart, LeadingZeroError(rawStart))
+    endID                  <- Either.cond(!(rawEnd.startsWith("0") && rawEnd.length > 1), rawEnd, LeadingZeroError(rawEnd))
+    bigStartID             <- Try(BigInt(startID)).toEither.left.map(th => InvalidNumber(startID))
+    bigEndID               <- Try(BigInt(endID)).toEither.left.map(th => InvalidNumber(endID))
+    (validA, validB)        = numberValidator(bigStartID) -> numberValidator(bigEndID)
+    invalidIDs              = (bigStartID to bigEndID).filter(numberValidator).toList
+    _                      <- Either.cond(invalidIDs.isEmpty, (), RepeatedNumbersError(invalidIDs))
+  yield bigStartID -> bigEndID
+
+  private val isInvalid: NumberValidator = id =>
+    val (s, length) = id.toString -> id.toString.length
+    length % 2 == 0 && s.take(length / 2) == s.drop(length / 2)
+
+  private val isInvalidPart2: NumberValidator = id =>
+    val (s, length) = id.toString -> id.toString.length
+    (1 to length / 2).exists(len => length % len == 0 && s == s.take(len) * (length / len))
+
+  private def sumInvalid(path: Path, validator: NumberValidator) =
+    ZStream
+      .fromJavaIterator(Files.readAllLines(path).iterator())
+      .flatMap(line => ZStream.fromIterable(line.split(",")))
+      .map(parseRange(validator))
+      .collect { case Left(RepeatedNumbersError(invalidIDs)) => invalidIDs }
+      .flatMap(ZStream.fromIterable)
+      .runSum
+
+  def program(path: Path) = for
+    _ <- sumInvalid(path, isInvalid).debug(s"Part 1")
+    _ <- sumInvalid(path, isInvalidPart2).debug(s"Part 2")
+  yield ()
