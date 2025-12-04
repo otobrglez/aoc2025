32206b4840db932fe4bafd84db7070b41e60b86a
Oto Brglez <otobrglez@gmail.com>
2025-12-04 10:07:16 +0100

    Making it nicer.

--- src/aoc/Main04.scala
+++ src/aoc/Main04.scala
@@ -10,39 +10,31 @@ type Roll = Char
 val roll: Roll = '@'; val space: Roll = '.'
 type Row = Long; type Column = Long
 
-final class Grid private (
-  private val grid: Map[(Row, Column), Option[Roll]] = Map.empty
-):
+final class Grid private (private val grid: Map[(Row, Column), Option[Roll]] = Map.empty):
   def find(r: Row, c: Column): Option[Roll]                     = grid.get((r, c)).flatten
   def size: (Row, Column)                                       = grid.keys.map(_._1).max -> grid.keys.map(_._2).max
   private def iterator: Iterator[((Row, Column), Option[Roll])] = grid.iterator
 
+  private def offsets(r: Row, c: Column): List[(Row, Column)] =
+    List(
+      // format: off
+      (r - 1, c - 1), (r - 1, c), (r - 1, c + 1),
+      (r, c - 1), (r, c + 1),
+      (r + 1, c - 1), (r + 1, c), (r + 1, c + 1)
+      // format: on
+    )
+
+  private def countNeighbors(r: Row, c: Column): Int =
+    offsets(r, c).count((r, c) => find(r, c).contains(roll))
+
   def numberOfAccessible(): Long =
-    iterator.collect { case (pos @ (r, c), Some(`roll`)) =>
-      pos -> List(
-        // format: off
-        (r - 1, c - 1), (r - 1, c), (r - 1, c + 1),
-        (r, c - 1), (r, c + 1),
-        (r + 1, c - 1), (r + 1, c), (r + 1, c + 1)
-        // format: on
-      ).map((r, c) => find(r, c).contains(roll)).count(_ == true)
-    }.count((_, count) => count < 4)
+    iterator.collect { case (pos @ (r, c), Some(`roll`)) => pos -> countNeighbors(r, c) }.count((_, count) => count < 4)
 
-  def mapAccessible(f: (Row, Column) => Option[Roll]): Seq[((Row, Column), Option[Roll])] =
-    iterator.toSeq.map {
-      case (pos @ (r, c), v @ Some(`roll`)) =>
-        val (_, cnt) = pos -> List(
-          // format: off
-          (r - 1, c - 1), (r - 1, c), (r - 1, c + 1),
-          (r, c - 1), (r, c + 1),
-          (r + 1, c - 1), (r + 1, c), (r + 1, c + 1)
-          // format: on
-        ).map((r, c) => find(r, c).contains(roll)).count(_ == true)
-        pos -> (if cnt < 4 then f(r, c) else v)
-      case (pos, v)                         => pos -> v
-    }
+  def mapAccessible(f: (Row, Column) => Option[Roll]): Seq[((Row, Column), Option[Roll])] = iterator.toSeq.map:
+    case (pos @ (r, c), v @ Some(`roll`)) if countNeighbors(r, c) < 4 => pos -> f(r, c)
+    case pos -> v                                                     => pos -> v
 
-  def withoutAccessibleRoles: Grid = Grid.from(mapAccessible((_, _) => None))
+  def withoutAccessible: Grid = Grid.from(mapAccessible((_, _) => None))
 
 object Grid:
   def fromPath(path: Path): Task[Grid] = {
@@ -59,22 +51,19 @@ object Grid:
     ZIO.foreachDiscard(0L to rows)(r => printLine((0L to cols).map(c => grid.find(r, c).getOrElse(space)).mkString))
 
   def removableCount(grid: Grid): Long =
-    var (g, sum) = grid -> 0L
-    while g.numberOfAccessible() > 0 do
-      sum += g.numberOfAccessible()
-      g = g.withoutAccessibleRoles
-    sum
+    @annotation.tailrec
+    def loop(g: Grid, acc: Long): Long =
+      val n = g.numberOfAccessible()
+      if n > 0 then loop(g.withoutAccessible, acc + n) else acc
+    loop(grid, 0L)
 
 object Main04 extends AOCApp:
   def program(path: Path) = for
-    _    <- printLine(s"Using ${path.toAbsolutePath}")
-    grid <- Grid.fromPath(path).tap(Grid.print)
-
+    grid              <- Grid.fromPath(path).tap(Grid.print)
     // Part 1: Number of neighbour roles less than 4
     numberOfPaperRoles = grid.numberOfAccessible()
     _                  = println(numberOfPaperRoles)
-
     // Part 2: Number of removable roles
-    removableCount = Grid.removableCount(grid)
-    _              = println(removableCount)
+    removableCount     = Grid.removableCount(grid)
+    _                  = println(removableCount)
   yield ()
