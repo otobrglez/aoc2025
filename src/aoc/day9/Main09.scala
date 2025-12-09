69ec74106be3b7b0403476333657b342d29f1ad1
Oto Brglez <otobrglez@gmail.com>
2025-12-09 11:54:07 +0100

    Using par

--- src/aoc/day9/Main09.scala
+++ src/aoc/day9/Main09.scala
@@ -3,24 +3,24 @@ package aoc.day9
 import aoc.AOCApp
 import aoc.Ops.*
 import zio.*
-
+import scala.collection.parallel.CollectionConverters.*
 import java.nio.file.Path
 
 private type Position = (x: Long, y: Long)
 extension (a: Position) def area(b: Position): Long = (Math.abs(b.x - a.x) + 1) * (Math.abs(b.y - a.y) + 1)
 
-def readPositions(path: Path): Task[List[Position]] =
+def readPositions(path: Path): Task[Vector[Position]] =
   path.linesStream
     .map(_.split(",", 2).toList.map(_.toLongOption))
     .collect[Position] { case List(Some(x), Some(y)) => x -> y }
     .runCollect
-    .map(_.toList)
+    .map(_.toVector)
 
-def findLargestArea(positions: List[Position]): Option[(Position, Position, Long)] =
+def findLargestArea(positions: Vector[Position]): (Position, Position, Long) =
   val pairs = for i <- positions.indices; j <- (i + 1) until positions.size yield positions(i) -> positions(j)
-  pairs.map((p1, p2) => (p1, p2, p1.area(p2))).maxByOption(_._3)
+  pairs.par.map((p1, p2) => (p1, p2, p1.area(p2))).maxBy(_._3)
 
-def isInside(pt1: Position, pt2: Position, polygon: List[Position]): Boolean =
+def isInside(pt1: Position, pt2: Position, polygon: Vector[Position]): Boolean =
   val (xmin, xmax) = Math.min(pt1.x, pt2.x) -> Math.max(pt1.x, pt2.x)
   val (ymin, ymax) = Math.min(pt1.y, pt2.y) -> Math.max(pt1.y, pt2.y)
 
@@ -35,15 +35,19 @@ def isInside(pt1: Position, pt2: Position, polygon: List[Position]): Boolean =
         (Math.min(y1, y2) <= ymin && ymin < Math.max(y1, y2) || Math.min(y1, y2) < ymax && ymax <= Math.max(y1, y2)))
     else throw new AssertionError("Expected to be rectilinear.")
 
-def findLargestArea2(polygon: List[Position]): Option[Long] =
+def findLargestArea2(polygon: Vector[Position]) =
   polygon
     .combinations(2)
-    .collect { case List(p1, p2) if isInside(p1, p2, polygon) => p1.area(p2) }
-    .maxOption
+    .toVector
+    .par
+    .collect { case Vector(p1, p2) if isInside(p1, p2, polygon) => p1.area(p2) }
+    .max
 
 object Main09 extends AOCApp:
   def program(path: Path) = for
-    positions <- readPositions(path)
-    _          = findLargestArea(positions).foreach((_, _, area) => println(s"Part 1: $area"))
-    _          = findLargestArea2(positions).foreach(area => println(s"Part 2: $area"))
+    positions     <- readPositions(path)
+    (_, _, result) = findLargestArea(positions)
+    _              = println(s"Part 1: $result")
+    result2        = findLargestArea2(positions)
+    _              = println(s"Part 2: $result2")
   yield ()
--- src/aoc/project.scala
+++ src/aoc/project.scala
@@ -1,3 +1,4 @@
 //> using dep dev.zio::zio:2.1.23
 //> using dep dev.zio::zio-streams:2.1.23
+//> using dep org.scala-lang.modules::scala-parallel-collections:1.2.0
 //> using options -deprecation -feature -unchecked -Wunused:all
