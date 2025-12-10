28cd361218c5c59bffa300f103b8e5e1fe3fdd50
Oto Brglez <otobrglez@gmail.com>
2025-12-10 12:26:20 +0100

    More fun.

--- src/aoc/day10/Z3SandboxApp.scala
+++ src/aoc/day10/Z3SandboxApp.scala
@@ -1,10 +1,10 @@
 package aoc.day10
 
-import com.microsoft.z3.*
 import zio.*
+import com.microsoft.z3.*
 
 // solve x > 2 and y < 10 and x + 2y = 7
-private def demoInteger = Z3Solver.withContext: ctx =>
+private def demoEquation = Z3Solver.withContext: ctx =>
   val (x, y) = ctx.mkIntConst("x") -> ctx.mkIntConst("y")
 
   val solved = ctx.mkSolver()
@@ -26,9 +26,29 @@ private def demoInteger = Z3Solver.withContext: ctx =>
     case status             =>
       println("No solution found: " + status)
 
+private def demoWear = Z3Solver.withContext: ctx =>
+  val tie   = ctx.mkBoolConst("tie")
+  val shirt = ctx.mkBoolConst("shirt")
+
+  val solver = ctx.mkSolver()
+  solver.add(ctx.mkOr(tie, shirt))                       // tie OR shirt
+  solver.add(ctx.mkImplies(tie, shirt))                  // tie IMPLIES shirt
+  solver.add(ctx.mkOr(ctx.mkNot(tie), ctx.mkNot(shirt))) // NOT tie OR NOT shirt
+
+  solver.check() match
+    case Status.SATISFIABLE =>
+      val model    = solver.getModel
+      val tieVal   = model.eval(tie, true)
+      val shirtVal = model.eval(shirt, true)
+      s"tie = $tieVal, shirt = $shirtVal"
+    case status             =>
+      s"No solution: $status"
+
+// Examples from: https://microsoft.github.io/z3guide/programming/Z3%20JavaScript%20Examples
 object Z3SandboxApp extends ZIOAppDefault:
   def program = for
-    _ <- demoInteger.debug("Demo integer:")
+    _ <- demoEquation
+    _ <- demoWear.debug("Wear")
     _  = println("Done!")
   yield ()
 
